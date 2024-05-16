d77863f20c120add519b8d2687f3ffe436c0183e
Douglas Pimentel Teixeira <douglaspt@gmail.com>
2024-05-16 17:00:56 -0300

    Movimentos possiveis de uma peça

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -46,6 +46,9 @@ public class ChessMatch {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("Não existe peça na posicao de origem!");
 		}
+		if (!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("Não existe movimentos possiveis para a peça escolhida!");
+		}
 		
 	}
 
